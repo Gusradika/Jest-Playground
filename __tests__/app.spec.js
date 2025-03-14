db163083870dcffed30a5eb4c85f232a848257bb
Gusradika <aditezo24@gmail.com>
2025-03-14 23:42:31 +0700

    jest.spyOn()

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -2,13 +2,17 @@ const crypto = require("crypto");
 const { getData } = require("../app");
 
 // Fake implementation
-jest.mock("crypto");
+// jest.mock("crypto");
 
 test("Fetch Data", async () => {
   // ResolvedValue
   //   crypto.randomBytes.mockResolvedValueOnce("bytes");
-  crypto.randomBytes.mockImplementationOnce(() => Promise.resolve("bytes"));
+  //   crypto.randomBytes.mockImplementationOnce(() => Promise.resolve("bytes"));
+
+  //   Tanpa inisasi dapat menggunakan jest.spyOn()
+  jest.spyOn(crypto, "randomBytes").mockResolvedValueOnce("bytes");
 
   const res = await getData();
   console.log(res);
+  expect(res).toBe("bytes");
 });
--- readme.md
+++ readme.md
@@ -3,4 +3,5 @@ Jest unit testing di init didalam Folder **tests** by default. dan isinya dapat 
 <ol>
     <li>1. Matcher untuk expeting ...</li>
     <li>2. Mock untuk implementasi dengan return Data dari (database mock), tidak boleh init database</li>
+    <li>3. Tanpa inisasi dapat menggunakan jest.spyOn(object, method) </li>
 </ol>
